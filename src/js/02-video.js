66e7056da60af9208121cc9dcf7fe827e746a67e
Denis Kovalsky <100243898+Den232323-goit@users.noreply.github.com>
2022-11-15 00:47:32 +0200

    222

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = throttle(data => {
+  localStorage.setItem(STORAGE_KEY, data);
+}, 1000);
+
+player.on('timeupdate', function () {
+  player.getCurrentTime().then(function (seconds) {
+    onPlay(seconds);
+  });
+});
+
+player.on('loaded', function () {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
 
+const input = document.querySelector('input');
+const STORAGE_KEY = 'feedback-form-state';
+
+let data = { email: '', message: '' };
+
+input.addEventListener(
+  'input',
+  throttle(event => {
+    if (event.target.nodeName === 'INPUT') {
+      data.email = event.target.value;
+    } else if (event.target.nodeName === 'TEXTAREA') {
+      data.message = event.target.value;
+    }
+    if (data) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+    }
+  }, 500)
+);
+if (localStorage.getItem(STORAGE_KEY)) {
+  data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+}
+email.value = data.email;
+message.value = data.message;
